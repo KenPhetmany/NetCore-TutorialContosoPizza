f5d972429b331c9eff345b7d0688bc909f86b000
Ken Phetmany <kenphetmanydev@gmail.com>
2022-04-30 21:29:58 +1000

    First Commit

---
+++ NetCore_RESTAPI/Models/Pizza.cs
@@ -0,0 +1,8 @@
+namespace ContosoPizza.Models;
+//Create Pizza model data
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IssGlutenFree { get; set; }
+}
