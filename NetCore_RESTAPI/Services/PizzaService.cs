703295df14916cb464d9a1e6fa9e3385f5ccb91c
Ken Phetmany <kenphetmanydev@gmail.com>
2022-05-01 18:50:13 +1000

    Updated Pizza Service - added more features

--- NetCore_RESTAPI/Services/PizzaService.cs
+++ NetCore_RESTAPI/Services/PizzaService.cs
@@ -25,6 +25,21 @@ public static class PizzaService
         pizza.Id = nextId++;
         Pizzas.Add(pizza);
     }
+    //Delete Pizza
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if (pizza is null)
+            return;
+        Pizzas.Remove(pizza);
+    }
 
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if (index == -1)
+            return;
+        Pizzas[index] = pizza;
+    }
 
 }
